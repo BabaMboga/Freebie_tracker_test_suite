075ecae3bba6ce227fed4d5d43524389e658a94a
Ayim William <odhisayim@hotmail.com>
2025-06-04 16:41:18 +0300

    one relationship left

--- tests/test_deliverables.py
+++ tests/test_deliverables.py
@@ -30,3 +30,30 @@ class TestRelationshipDeliverables:
 
     def test_freebie_company_relationship(self, sample_freebies, sample_companies):
         """Freebie.company returns company instance"""
+        freebie = sample_freebies["hildah_tshirt"]
+        assert freebie.company == sample_companies["google"]
+
+    def test_company_freebie_relationships(self, sample_freebies, sample_companies):
+        """company.freebies returns collection of freebies"""
+        google = sample_companies["google"]
+        assert len(google.freebies) == 2
+        assert hasattr(google, 'freebies')
+
+    def test_company_devs_relationship(self, sample_freebies, sample_companies):
+        """company.devs returns collection of devs"""
+
+        google = sample_companies["google"]
+        assert len(google.devs) == 2
+        dev_names = [d.name for d in google.devs]
+        assert "Alice" in dev_names and "Bob" in dev_names
+
+    def test_dev_freebies_relationship(self, sample_freebies, sample_devs):
+        """dev.freebies returns collection of freebies"""
+        hildah = sample_devs["hildah"]
+        assert len(hildah.freebies) == 2
+        assert hasattr(hildah, 'freebies')
+
+    def test_dev_companies_relationship(self, sample_freebies, sample_devs):
+        pass
+
+    
